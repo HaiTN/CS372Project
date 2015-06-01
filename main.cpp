94725c3099631df8f5091b1e921f12fe6d571788
Hai <nguyehai@onid.oregonstate.edu>
2015-06-01 16:10:57 -0700

    Added generateGraph function.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+struct Edge {
+    int node;
+    int location;
+    int weight;
+};
+
+struct Graph {
+    int vertice;
+    int edge;
+
+    struct Edge* edge;
+};
+
+struct Graph* generateGraph(int vertice, int edge) {
+    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+    graph->vertice = vertice;
+    graph->edge = edge;
+
+    graph->edge = (struct Edge*) malloc(graph->edge * sizeof(struct Edge));
+
+    return graph;
+};
+
+int main() {
+
     return 0;
 }
