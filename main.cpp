c9323814b545c347d8f42447b1fd8d54b93a4a2a
Hai <nguyehai@onid.oregonstate.edu>
2015-06-06 12:46:27 -0700

    Reworked most of the structure. Used heap.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+#include <limits.h>
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
 #include <math.h>
 #include <time.h>
@@ -7,138 +9,332 @@
 
 using namespace std;
 
-struct Edge {
-    int node;
-    int location;
-    int weight;
-    int distance;
-    Edge *next;
-    Edge *prev;
+class Graph {
+	int** adjust_matrix;
+	size_t size;
+	size_t max_size;
+
+	void delete_matrix() {
+		for (size_t count = 0; count < this->max_size; ++count) {
+			delete[] this->adjust_matrix[count];
+		}
+		delete[] this->adjust_matrix;
+	}
+
+	void resize(size_t newSize) {
+		int ** newMatr;
+		copy_matrix(this->adjust_matrix, &newMatr, this->size, newSize);
+		delete_matrix();
+		this->adjust_matrix = newMatr;
+	}
+
+	void copy_matrix(int** src, int*** dest, size_t size, size_t max_size) {
+		*dest = new int*[this->max_size];
+		for (size_t count = 0; count < this->max_size; ++count) {
+			*dest[count] = new int[this->max_size];
+		}
+		for (size_t count = 0; count < this->max_size; ++count)
+			for (size_t count2 = 0; count2 < this->max_size; ++count2) {
+				if (count < size && count2 < size)
+					*dest[count][count2] = src[count][count2];
+				else
+					*dest[count][count2] = 0;
+			}
+	}
+
+public:
+	Graph(size_t max_size = 20) {
+		this->size = 0;
+		this->max_size = max_size;
+		this->adjust_matrix = new int*[this->max_size];
+		for (size_t count = 0; count < this->max_size; ++count) {
+			this->adjust_matrix[count] = new int[this->max_size];
+			for (size_t count2 = 0; count2 < this->max_size; ++count2) {
+				this->adjust_matrix[count][count2] = 0;
+			}
+		}
+	}
+	Graph(const Graph& src) {
+		this->copy_matrix(src.adjust_matrix, &this->adjust_matrix,src.size, src.max_size);
+	}
+	~Graph() {
+		this->delete_matrix();
+	}
+
+	void addNodes(int numOfNodes) {
+		if (this->size + 1 >= this->max_size)
+		{
+			resize(this->max_size * 2);
+		}
+		this->size += numOfNodes;
+	}
+
+	void addEdge(size_t from, size_t to, int weight) {
+		if (from < size && to < size)
+			this->adjust_matrix[from][to] = weight;
+	}
+
+	void removeEdge(size_t from, size_t to) {
+		if (from < size && to < size)
+			this->adjust_matrix[from][to] = 0;
+	}
+
+	int hasEdge(size_t from, size_t to) {
+		if (from < size && to < size)
+			return this->adjust_matrix[from][to];
+		return false;
+	}
+
+	void print() {
+		cout << setw(3)<< "  ";
+		for (size_t count = 0; count < this->size; ++count) {
+			cout << setw(3) << count;
+		}
+		cout << endl;
+		cout << endl;
+		for (size_t count = 0; count < this->size; ++count) {
+			cout << setw(2)<< count << " ";
+			for (size_t count2 = 0; count2 < this->size; ++count2) {
+				cout << setw(3) << this->adjust_matrix[count][count2];
+			}
+			cout << endl;
+		}
+	}
+
+	size_t GetSize() {
+		return this->size;
+	}
+
 };
 
-struct Graph {
-    int v;
-    int e;
 
-    struct Edge* edge;
+template<class T>
+class Heap
+{
+	struct HeapNode {
+		HeapNode(){}
+		int weight;
+		T val;
+	};
+
+	HeapNode * nodes;
+	size_t size;
+	size_t max_size;
+
+	unsigned LeftChildIndex(int index)const {
+		return index * 2;
+	}
+	unsigned RightChildIndex(int index)const {
+		return index * 2 + 1;
+	}
+	unsigned ParentIndex(int index)const {
+		if (index > 1)
+			return index / 2;
+		else
+			return 1;
+	}
+
+	void Swap(int left, int right) {
+		HeapNode tmp;
+		tmp = this->nodes[left];
+		this->nodes[left] = this->nodes[right];
+		this->nodes[right] = tmp;
+	}
+
+	bool Resize(size_t newSize) {
+		if (newSize < size)
+			return false;
+
+		HeapNode* newArr = new HeapNode[newSize + 1];
+		if (!newArr) {
+			return false;
+		}
+
+		for (unsigned i = 1; i <= size; ++i) {
+			newArr[i] = this->nodes[i];
+		}
+
+		delete[] this->nodes;
+		this->nodes = newArr;
+
+		return true;
+	}
+public:
+
+	Heap() {
+		this->size = 0;
+		this->max_size = 20;
+		this->nodes = new HeapNode[this->max_size + 1];
+	}
+	~Heap() {
+		delete[] this->nodes;
+	}
+	bool Push(const T& val, int weight) {
+		if (this->IsEmpty()) {
+			this->nodes[1].val = val;
+			this->nodes[1].weight = weight;
+			this->size++;
+			return true;
+		}
+
+		if (size + 1 <= max_size) {
+			this->Resize(max_size * 2);
+		}
+
+		int iter = size + 1;
+		this->nodes[iter].val = val;
+		this->nodes[iter].weight = weight;
+		this->size++;
+
+		while (iter > 1 && this->nodes[iter].weight < this->nodes[ParentIndex(iter)].weight) {
+			this->Swap(iter, ParentIndex(iter));
+			iter = ParentIndex(iter);
+		}
+
+		return true;
+	}
+
+	T Pop() {
+
+		T tmp = this->nodes[1].val;
+		this->Swap(1, this->size);
+		this->size--;
+
+		int iter = 1;
+
+		while ((this->nodes[iter].weight > this->nodes[LeftChildIndex(iter)].weight && LeftChildIndex(iter) <= size)
+			|| (this->nodes[iter].weight > this->nodes[RightChildIndex(iter)].weight && RightChildIndex(iter) <= size))
+		{
+			if (this->nodes[iter].weight > this->nodes[LeftChildIndex(iter)].weight && LeftChildIndex(iter) <= size
+				&& this->nodes[iter].weight > this->nodes[RightChildIndex(iter)].weight && RightChildIndex(iter) <= size)
+			{
+				if (this->nodes[LeftChildIndex(iter)].weight > this->nodes[RightChildIndex(iter)].weight) {
+					this->Swap(iter, RightChildIndex(iter));
+					iter = RightChildIndex(iter);
+				}
+				else {
+					this->Swap(iter, LeftChildIndex(iter));
+					iter = LeftChildIndex(iter);
+				}
+			}
+			else if (this->nodes[iter].weight > this->nodes[LeftChildIndex(iter)].weight) {
+				this->Swap(iter, LeftChildIndex(iter));
+				iter = LeftChildIndex(iter);
+			}
+			else {
+				this->Swap(iter, RightChildIndex(iter));
+				iter = RightChildIndex(iter);
+			}
+		}
+
+		if (max_size > 2 * size)
+			this->Resize(max_size / 2);
+		return tmp;
+	}
+
+	bool IsEmpty()const {
+		return this->size <= 0;
+	}
+
+	size_t GetSize()const {
+		return this->size;
+	}
+
+	int GetTopWeight() {
+		return this->nodes[1].weight;
+	}
+
+	void print() {
+		for (int i = 1; i <= this->size; ++i) {
+			cout << this->nodes[i].val << endl;
+		}
+	}
 };
 
-struct Graph* generateGraph(int vertex, int edge) {
-    struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
-    graph->v = vertex;
-    graph->e = edge;
+/*************
+Dijkstra Implementation
+Input: graph, and initial node
+*************/
 
-    graph->edge = (struct Edge*) malloc(graph->e * sizeof(struct Edge));
 
-    return graph;
-};
+void dijkstra(Graph& gr, int initial)
+{
+	size_t size = gr.GetSize();
+	int* dist = new int[size];
 
-int pop(Edge *head) {
-    int location, weight, distance, node;
-    Edge *temp;
+	Heap<int> heap;
+	heap.Push(initial, 0);
 
-    while(head->next != NULL)
-        head = head->next;
+	for (size_t count = 0; count < size; ++count) {
+		dist[count] = INT_MAX;
+	}
+	dist[initial] = 0;
 
-    node = head->node;
-    location = head->location;
-    weight = head->weight;
-    distance = head->distance;
+	while (!heap.IsEmpty())
+	{
+		int crnWeight = heap.GetTopWeight();
+		int crnElem = heap.Pop();
+		for (int count = 0; count < size; ++count) {
+			if (gr.hasEdge(crnElem, count)) {
+				if (dist[count] > crnWeight + gr.hasEdge(crnElem, count)) {
+					dist[count] = crnWeight + gr.hasEdge(crnElem, count);
+					heap.Push(count, dist[count]);
+				}
+			}
+		}
+	}
 
-    temp = new Edge;
-    delete temp;
-    return node, location, weight, distance;
+	//print result
+	for (int i = 0; i < size; ++i) {
+        if(dist[i] == 2147483647)
+            cout << "From node[" << initial << "] to [" << i << "]:" << "NO PATH FOUND" << endl;
+        else
+            cout << "From node[" << initial << "] to [" << i << "]:" << dist[i] << endl;
+	}
+	delete[] dist;
 }
 
-int numNeighbors(Edge *head) {
-    int num_neighbors;
-    Edge *tempnext;
-    Edge *tempprev;
+void inputGraph(Graph& gr)
+{
+	int node;
+	int destination;
+	int n, weight;
+	//cout << "Number of Nodes(Maximum 20):";
+	//cin >> n;
+	n = 20;
+	gr.addNodes(n);
+	//cout << "Number of edges:";
+	//cin >> n;
+	n = 1000000; //set to the amount of edges
+	while (n > 0)
+    {
+		node = rand() % n;
+		destination = rand() % n;
+		weight = rand() % 100;
 
-    tempnext = head->next;
-    tempprev = head->prev;
+       // cout << "Node: " << from
+         //   << "    -----     Destination: " << to
+           // << "    -----     Weight: " << weight << endl;
 
-    if(tempnext->node == head->location)
-        num_neighbors++;
-    if(tempprev->node == head->location)
-        num_neighbors++;
-
-    return num_neighbors;
+		gr.addEdge(node, destination, weight);
+		--n;
+	}
 }
 
-int dijkstraAlg(struct Graph* graph, int destination, int node, int vertex, int edge) {
-    graph->edge->distance = 0;
-    graph->edge->prev = NULL;
+int main()
+{
+    clock_t t;
+    t = clock();
+	Graph graph;
+	inputGraph(graph);
+	graph.print();
 
-    cout << "Before Search 1" << endl;
+	int n = 0;
+	//cout << "initial node:" << endl;
+	//cin >> n;
 
-    struct Graph* visited = generateGraph(vertex, edge);
-    struct Graph* sequence;
+	dijkstra(graph, n);
+	//cout << ((float)t/CLOCKS_PER_SEC) << " seconds." << endl;
 
-    cout << "Before Search 2" << endl;
-    for(int i = 0; i < vertex; i++) {
-        if(graph->edge[i].node != graph->edge[node].node) {
-            //cout << "Initializing step " << i << "..." << endl;
-            graph->edge[i].distance = 99999999;
-            graph->edge[i].prev = NULL;
-            //cout << "Edge " << i << " distance = " << graph->edge[i].distance << endl;
-        }
-
-        //cout << "Out of if statement " << i << "..." << endl;
-        visited->edge[i].node = graph->edge[i].node;
-        //cout << "Already visited: " << visited->edge[i].node << endl;
-        //cout << "After visited" << endl;
-    }
-
-    cout << "End Search 2" << endl;
-
-    while(visited->edge->node != NULL) {
-        cout << "Before Pop" << endl;
-        graph->edge[0].node = destination;
-        pop(graph->edge);
-        cout << "After Pop" << endl;
-
-        for(int i = 0; i < (vertex-1); i++) {
-            cout << "Before alt" << endl;
-            int alt = graph->edge[0].distance + graph->edge[i].weight;
-            cout << endl;
-            cout << "Alt = " << alt << endl << endl;
-            cout << "Edge " << i << " distance = " << graph->edge[i].distance << endl;
-
-            if(alt < graph->edge[i].distance) {
-                graph->edge[i].distance = alt;
-                    cout << "Edge " << i << " distance = " << graph->edge[i].distance << endl;
-                //graph->edge[0].node = graph->edge[i].next;
-            }
-        }
-    }
-
-    return graph->edge->distance;
-}
-
-int main() {
-    int v, e;
-    struct Graph* graph = generateGraph(v, e);
-
-    cout << "Insert amount of vertices: ";
-    cin >> v;
-    cout << "Insert amount of edges: ";
-    cin >> e;
-
-    cout << "Edges: " << e << " Vertices: " << v << endl;
-    cout << "Generating graph..." << endl;
-
-    for(int x = 0; x < e; x++) {
-        graph->edge[x].node = rand() % v;
-        graph->edge[x].location = rand() % v;
-        graph->edge[x].weight = rand() % 100;
-
-        cout << "Node: " << graph->edge[x].node
-            << "    -----     Destination: " << graph->edge[x].location
-            << "    -----     Weight: " << graph->edge[x].weight << endl;
-    }
-
-    dijkstraAlg(graph, 10, 0, v, e);
-
-    return 0;
+	return 0;
 }
